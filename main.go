99f939c75d50bd9c95a9e94ed6fdbdbc3a9c96eb
Izaak Dale <izaakdaledev@gmail.com>
2023-12-12 00:53:21 -0800

    accepting interface to handler

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 )
 
-func main() {
+type CtxKey string
 
-	mux := http.NewServeMux()
+const connectionID CtxKey = "connectionID"
 
-	// allow origins
-	upgrader := websocket.Upgrader{CheckOrigin: func(r *http.Request) bool {
-		return true
+type WebsocketUpgrader interface {
+	Upgrade(w http.ResponseWriter, r *http.Request, responseHeader http.Header) (*websocket.Conn, error)
+}
 
-		// org := r.Header.Get("Origin")
-		// return org == "http://localhost:5173"
-	}}
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
+func connect(wsu WebsocketUpgrader) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		conn, err := wsu.Upgrade(w, r, nil)
 		if err != nil {
 			log.Print("upgrade failed: ", err)
 			return
@@ -33,21 +33,52 @@ func main() {
 
 		toSend := strings.Split("this is a backend message", " ")
 
+		cID, ok := r.Context().Value(connectionID).(uuid.UUID)
+		if !ok {
+			http.Error(w, "some error", http.StatusInternalServerError)
+			return
+		}
+
 		var i int
 		for {
 			send := struct {
-				Message string `json:"message,omitempty"`
-			}{toSend[i%len(toSend)]}
+				ID      uuid.UUID `json:"id,omitempty"`
+				Message string    `json:"message,omitempty"`
+			}{
+				cID,
+				toSend[i%len(toSend)],
+			}
 
 			if err := conn.WriteJSON(send); err != nil {
 				// probably due to client close, break to trigger deferred close of conn
 				break
 			}
 
-			time.Sleep(time.Second / 4)
+			time.Sleep(10 * time.Second)
 			i++
 		}
-	})
+	}
+}
 
-	http.ListenAndServe(fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT")), mux)
+func main() {
+	mux := http.NewServeMux()
+
+	// allow origins
+	upgrader := websocket.Upgrader{CheckOrigin: func(r *http.Request) bool {
+		return true
+		// org := r.Header.Get("Origin")
+		// return org == "http://localhost:5173"
+	}}
+
+	mux.HandleFunc("/", connect(&upgrader))
+
+	srv := http.Server{
+		Addr:    fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT")),
+		Handler: mux,
+		ConnContext: func(ctx context.Context, c net.Conn) context.Context {
+			return context.WithValue(ctx, connectionID, uuid.New())
+		},
+	}
+
+	srv.ListenAndServe()
 }
